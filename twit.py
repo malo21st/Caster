4336a1f1fb8ca6eac207f0862b401d1c67c0e1e9
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-24 08:37:23 +0900

    Create twit.py

---
+++ twit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
