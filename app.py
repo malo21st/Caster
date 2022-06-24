8378f76015cafc4b4259ac56838257c31030c1ee
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-24 13:09:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 
+## create Client
+@st.cache()
+def ClientInfo():
+    client = tweepy.Client(bearer_token    = st.secrets["BEARER_TOKEN"],
+                           consumer_key    = st.secrets["API_KEY"],
+                           consumer_secret = st.secrets["API_SECRET"],
+                           access_token    = st.secrets["ACCESS_TOKEN"],
+                           access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"],
+                          )
+    return client
+client = ClientInfo()
+
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
@@ -31,4 +43,6 @@ st.dataframe(df)
 msg_idx = st.radio("Select message.", df_index, horizontal=True)
 init_msg = f"{df.loc[msg_idx, 'title']}\n{df.loc[msg_idx, 'text']}\n{df.loc[msg_idx, 'tag']}"
 message = st.text_area("edit message.", value=init_msg) #, height=100)
-st.write(message)
+
+if st.button('Cast tweet'):
+     client.create_tweet(text=message)
