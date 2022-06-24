d07dc8221e2c114620c3ca75d21580d1ef56fc6c
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-24 12:42:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+import pandas as pd
 
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+@st.cache()
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+df = pd.DataFrame(rows, columns=["title", "text", "tag"])
+df_index = df.index
+
+# Print results.
+st.title("Twit : easy tweet apli")
+
+st.dataframe(df)
+
+msg_idx = st.radio("Select message.", df_index, horizontal=True)
+init_msg = f"{df.loc[msg_idx, 'title']}\n{df.loc[msg_idx, 'text']}\n{df.loc[msg_idx, 'tag']}"
+message = st.text_area("edit message.", value=init_msg) #, height=100)
+st.write(message)
