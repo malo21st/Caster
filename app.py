34126d24fa61054f1257cf6b1e46fdc00e3869ca
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-24 15:08:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,28 @@ import tweepy
 from io import BytesIO
 from PIL import Image
 
+consumer_key    = st.secrets["API_KEY"]
+consumer_secret = st.secrets["API_SECRET"]
+access_token    = st.secrets["ACCESS_TOKEN"]
+access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"]
+
 ## create Client
-def ClientInfo():
-    client = tweepy.Client(bearer_token    = st.secrets["BEARER_TOKEN"],
-                           consumer_key    = st.secrets["API_KEY"],
-                           consumer_secret = st.secrets["API_SECRET"],
-                           access_token    = st.secrets["ACCESS_TOKEN"],
-                           access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"],
-                          )
-    return client
-client = ClientInfo()
+# def ClientInfo():
+#     client = tweepy.Client(bearer_token    = st.secrets["BEARER_TOKEN"],
+#                            consumer_key    = st.secrets["API_KEY"],
+#                            consumer_secret = st.secrets["API_SECRET"],
+#                            access_token    = st.secrets["ACCESS_TOKEN"],
+#                            access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"],
+#                           )
+#     return client
+# client = ClientInfo()
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+ 
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
@@ -38,12 +50,17 @@ df = pd.DataFrame(rows, columns=["title", "text", "tag"])
 df_index = df.index
 
 # Print results.
-
-
 st.title("Caster - tweet so easy -")
-picture_data = st.file_uploader("Choose a picture", type=['png', 'jpg', 'PNG', 'JPG', 'jpeg'])
+picture_data = st.file_uploader("Choose a picture", type=['png', 'jpg', 'jpeg'])
 if picture_data:
     st.image(picture_data)
+    # Save image in-memory
+    im = Image.fromarray(picture_data)
+    b = BytesIO()
+    im.save(b, "PNG")
+    b.seek(0)
+    
+    
 st.dataframe(df)
 
 msg_idx = st.radio("Select message.", df_index, horizontal=True)
@@ -51,4 +68,9 @@ init_msg = f"{df.loc[msg_idx, 'title']}\n{df.loc[msg_idx, 'text']}\n{df.loc[msg_
 message = st.text_area("edit message.", value=init_msg) #, height=100)
 
 if st.button('Cast tweet'):
-     client.create_tweet(text=message)
+#      client.create_tweet(text=message)
+    # Upload media to Twitter APIv1.1
+    ret = api.media_upload(filename="dummy_string", file=b)
+
+    # Attach media to tweet
+    api.update_status(media_ids=[ret.media_id_string], status=message)
