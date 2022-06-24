3b3d6af92d8d8e5396ef0bc69409caba8b4dbf3e
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-24 17:45:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"]
 # client = ClientInfo()
 
 # OAuth process, using the keys and tokens
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-auth = tweepy.OAuth1UserHandler(
-    consumer_key, consumer_secret, access_token, access_token_secret
-)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+# auth = tweepy.OAuth1UserHandler(
+#     consumer_key, consumer_secret, access_token, access_token_secret
+# )
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 
