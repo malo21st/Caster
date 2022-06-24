c3d3dee74e33fa04b7cd14ede0f8fa21f3e699b3
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-24 16:18:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ access_token_secret = st.secrets["ACCESS_TOKEN_SECRET"]
 # client = ClientInfo()
 
 # OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
  
 # Creation of the actual interface, using authentication
@@ -69,9 +69,9 @@ message = st.text_area("edit message.", value=init_msg) #, height=100)
 
 if st.button('Cast tweet'):
 #      client.create_tweet(text=message)
-    api.update_status_with_media(status = message, filename="dummy", file = fp)
-#     # Upload media to Twitter APIv1.1
-#     ret = api.media_upload(filename="dummy", file=fp)
+#     api.update_status_with_media(status = message, filename="dummy", file = fp)
+    # Upload media to Twitter APIv1.1
+    ret = api.media_upload(filename="dummy", file=fp)
 
-#     # Attach media to tweet
-#     api.update_status(media_ids=[ret.media_id_string], status=message)
+    # Attach media to tweet
+    api.update_status(media_ids=[ret.media_id_string], status=message)
