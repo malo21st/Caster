5481b5f10a7293c33e03e5d17531dcc56232020b
malo21st <44739330+malo21st@users.noreply.github.com>
2022-06-25 14:58:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ df_index = df.index
 
 # Print results.
 st.title("Caster - tweet so easy -")
-picture_data = st.file_uploader("Select a picture", type=['png', 'jpg', 'jpeg'])
+picture_data = st.file_uploader("Select a picture :", type=['png', 'jpg', 'jpeg'])
 if picture_data:
     img = Image.open(picture_data)
     #リサイズ&圧縮
@@ -54,9 +54,9 @@ if picture_data:
     
 st.dataframe(df)
 
-msg_idx = st.radio("Select tweet.", df_index, horizontal=True)
+msg_idx = st.radio("Select tweet :", df_index, horizontal=True)
 init_msg = f"{df.loc[msg_idx, 'title']}\n{df.loc[msg_idx, 'text']}\n\n{df.loc[msg_idx, 'tag']}"
-message = st.text_area("edit tweet.", value=init_msg, height=200)
+message = st.text_area("Edit tweet :", value=init_msg, height=200)
 
 if st.button('Cast tweet'):
     if picture_data:
